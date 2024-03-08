7cc9e4699fe0a639bf90fdea31580b65a2004582
Konpwomi <patiphan.arp@gmail.com>
2024-03-08 14:50:10 +0700

    add describe

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>Click on the Vite and React logos to learn more Click on the Vite and React logos to learn more Click on the Vite and React logos to learn more</p>
     </>
   )
 }
