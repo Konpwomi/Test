be22da8282d301796bd105e52c64d8cc4227cede
Konpwomi <patiphan.arp@gmail.com>
2024-03-08 14:53:53 +0700

    add detail 4

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,8 @@ function App() {
       <p>Click on the Vite and React logos to learn more Click on the Vite and React logos to learn more Click on the Vite and React logos to learn more</p>
       <p>Click on the Vite and React logos to learn more Click on the Vite and React logos to learn more Click on the Vite and React logos to learn more</p>
       <p>Click on the Vite and React logos to learn more Click on the Vite and React logos to learn more Click on the Vite and React logos to learn more</p>
-    
+      <p>Click on the Vite and React logos to learn more Click on the Vite and React logos to learn more Click on the Vite and React logos to learn more</p>
+
     </>
   )
 }
